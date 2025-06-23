e7ae07f6b620f132fdd3348d429f886321b9133b
Senac Hub - Sala 203 <hubsala203@aluno.ms.senac.br>
2025-06-23 19:46:53 -0400

    first commit

---
+++ lib/components/botao_pre_login.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class BotaoPreLogin extends StatelessWidget {
+  IconData icon;
+  String text;
+
+  BotaoPreLogin(this.icon, this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        // padding: EdgeInsets.all(15),
+        width: MediaQuery.of(context).size.width * 0.3,
+        height: 100,
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: 
+          [
+            Icon(icon, color: Color.fromARGB(255, 236, 9, 0),),
+            SizedBox(height: 5,),
+            Text(text, style: TextStyle(color: Colors.grey), textAlign: TextAlign.center,)
+          ]
+        ),
+      ),
+    );
+  }
+}
